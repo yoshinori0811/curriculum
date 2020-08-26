5dc7e459dd22bae1e00a4bb53000b57100cf2337
yoshinori0811 <grapemix000@gmail.com>
2020-08-26 11:44:16 +0900

    1章チェックテスト課題の修正
    Check.javaの修正

--- javastudy/Check1/Check.java
+++ javastudy/Check1/Check.java
@@ -20,13 +20,13 @@ public class Check{
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < data.length-1; i++) {
-            for (int j = data.length-1; j > i; j--) {
+        for (int i = 0; i < data.length - 1; i ++) {
+            for (int j = data.length - 1; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
-                if(data[j - 1] < data[j]){
+                if(data[j - 1] > data[j]){
                   int box = data[j - 1];
                   data[j - 1] = data[j];
                   data[j] = box;
